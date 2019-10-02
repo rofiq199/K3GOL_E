65b0a33677c9111fa64daedafab213f0f8ed9df4
rofiq199 <55471142+rofiq199@users.noreply.github.com>
2019-10-02 05:23:41 -0700

    Update home.php

--- Rofiq/home.php
+++ Rofiq/home.php
@@ -3,9 +3,9 @@
     session_start();
      ob_end_clean();
     if(isset($_SESSION["username"])){
-    echo "<h2>Halo </h2>";
-    echo  $_SESSION['username'];
-    echo "<a href='logout.php'>Logout</a>";
+        echo "<h2>Halo </h2>";
+        echo  $_SESSION['username'];
+        echo "<a href='logout.php'>Logout</a>";
     }else{
         echo header("location:form_login.php");
     }
