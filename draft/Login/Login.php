ac1115cf72a1280d96fbe38acd5aa13af880e362
rofiq199 <55471142+rofiq199@users.noreply.github.com>
2019-10-16 14:49:13 +0700

    update

--- draft/Login/Login.php
+++ draft/Login/Login.php
@@ -32,7 +32,7 @@
 	<div class="limiter">
 		<div class="container-login100" style="background-image: url('images/bg-02.jpg');">
 			<div class="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
-				<form class="login100-form validate-form flex-sb flex-w">
+				<form action="home.php" method="POST"class="login100-form validate-form flex-sb flex-w">
 					<span class="login100-form-title p-b-53">
 						Sign In With
 					</span>
@@ -67,12 +67,12 @@
 						</a>
 					</div>
 					<div class="wrap-input100 validate-input" data-validate = "Password is required">
-						<input class="input100" type="password" name="pass" >
+						<input class="input100" type="password" name="password" >
 						<span class="focus-input100"></span>
 					</div>
 
 					<div class="container-login100-form-btn m-t-17" >
-					<button class="login100-form-btn">
+					<button type="submit" class="login100-form-btn"> 
 							Sign In
 						</button>
 					</div>
--- draft/Login/ceklogin.php
+++ draft/Login/ceklogin.php
@@ -7,10 +7,10 @@ include 'koneksi.php';
 
 // menangkap data yang dikirim dari form
 $username = $_POST['username'];
-$pass = md5($_POST['password']);
+$password = $_POST['password'];
 
 // menyeleksi data admin dengan username dan password yang sesuai
-$data = mysqli_query($koneksi,"select * from tb_user where username='$username' and password='$pass'");
+$data = mysqli_query($koneksi,"select * from data_barber where username_bs='$username' and password_bs='$password'");
 
 // menghitung jumlah data yang ditemukan
 $cek = mysqli_num_rows($data);
