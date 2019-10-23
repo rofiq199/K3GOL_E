8bd452d29fb6a9da159da74592b9898c30689405
dodhy <dodhyk29@gmail.com>
2019-10-23 15:22:56 +0700

    baru

--- barberian/caribarber.php
+++ barberian/caribarber.php
@@ -51,6 +51,13 @@
   <!-- Section: Blog v.3 -->
 
 <div class="container">
+<div class="input-group md-form form-sm form-2 pl-0 my-5">
+  <input class="form-control my-0 py-1 amber-border" type="text" placeholder="Search" aria-label="Search">
+  <div class="input-group-append">
+    <span class="input-group-text amber lighten-3" id="basic-text1"><i class="fas fa-search text-grey"
+        aria-hidden="true"></i></span>
+  </div>
+</div>
 <section class="my-5 barber">
   <hr class="my-4">
 <?php
@@ -176,5 +183,6 @@ while($rows=mysqli_fetch_object($result)){
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/6b0af4bcb3.js" crossorigin="anonymous"></script>
     </body>
   </html>
