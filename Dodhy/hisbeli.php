c434ce93d5d88325cd16cb96f0a254183c58cd05
dodhy <dodhyk29@gmail.com>
2019-11-27 10:16:16 +0700

    baru

---
+++ Dodhy/hisbeli.php
@@ -0,0 +1,291 @@
+<?php
+  $koneksi = mysqli_connect("localhost","root","","barberian");
+  // Check connection
+  if (mysqli_connect_errno()){
+    echo "Koneksi database gagal : " . mysqli_connect_error();
+  }
+  session_start();
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="script.js"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Viga&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Karla&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script&display=swap" rel="stylesheet">
+
+    <!--My CSS-->
+    <link rel="stylesheet" href="hisbeli.css">
+  
+    <title>Barberian</title>
+  </head>
+  <body>
+
+  <!--Navbar-->
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top sticky">
+    <div class="container">
+      <a class="navbar-brand" href="#">Barberian</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <div class="navbar-nav ml-auto">
+          <a class="nav-item nav-link js-scroll-trigger" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link js-scroll-trigger" href="#tentang_kami">Tentang Kami</a>
+          <div class="dropdown">
+            <a class="nav-item nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Fitur Kami
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="katalog.php">Katalog</a>
+                    <a class="dropdown-item" href="caribarber.php">Cari Barbershop</a>
+                    <a class="dropdown-item" href="halproduk.php">Produk</a>
+                  </div>
+          </div>     
+          <?php if(!isset($_SESSION['username'])){ ?>
+          <div class="dropdown">
+            <a class="nav-item nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Masuk
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="" data-toggle="modal" data-target="#darkModalForm">Masuk Sebagai Customer</a>
+                  <a class="dropdown-item" href="" data-toggle="modal" data-target="#modalLoginForm">Masuk Sebagai Barbershop</a>
+                  <a class="dropdown-item" href="" data-toggle="modal" data-target="#modalForm">Masuk Sebagai Barberman</a>
+                  </div>
+            </div>
+            <?php }else{
+          ?>
+          <div class="dropdown">
+            <a class="nav-item nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <?php echo $_SESSION['username']?>
+                  </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href='profilcs.php'>Profil</a>
+                  <a class="dropdown-item" href="hisbeli.php" >History Order</a>
+                  <a class="dropdown-item" href="lihat_antrian.php" >Lihat Antrian</a>
+                  <a class="dropdown-item" href="logout.php" >Logout</a>
+                </div>
+            </div>
+            <?php }
+            ?>
+          </div>
+        </div>
+    </div>
+    </nav>
+  <!-- akhir Navbar -->
+  
+<!-- Section: Features v.1 -->
+<!-- akhir Fitur -->
+<!-- Modal -->
+<div class="modal fade" id="darkModalForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog form-dark" role="document">
+    <!--Content-->
+    <form action="/K3GOL_E/barberian/Login/ceklogin.php " method="POST">
+    <div class="modal-content card card-image" id="popup">
+      <div class="text-white rgba-stylish-strong py-5 px-5 z-depth-4">
+        <!--Header-->
+        <div class="modal-header text-center pb-4">
+          <h3 class="modal-title w-100 white-text font-weight-bold" id="myModalLabel"><strong>SIGN</strong> <a
+              class="green-text font-weight-bold"><strong> IN</strong></a></h3>
+          <button type="button" class="close white-text" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <!--Body-->
+        <div class="modal-body">
+          <!--Body-->
+          <div class="md-form mb-5">
+            <input type="username" name="username" class="form-control validate white-text">
+            <label data-error="wrong" data-success="right" for="Form-email5">Your email</label>
+          </div>
+
+          <div class="md-form pb-3">
+            <input type="password" name="password" class="form-control validate white-text">
+            <label data-error="wrong" data-success="right" for="Form-pass5">Your password</label>
+            <div class="form-group mt-4">
+              <input class="form-check-input" type="checkbox" id="checkbox624">
+              <label for="checkbox624" class="white-text form-check-label">Accept the<a href="#" class="green-text font-weight-bold">
+                  Terms and Conditions</a></label>
+            </div>
+          </div>
+
+          <!--Grid row-->
+          <div class="row d-flex align-items-center mb-4">
+
+            <!--Grid column-->
+            <div class="text-center mb-3 col-md-12">
+              <button type="submit" class="btn btn-success btn-block btn-rounded z-depth-1">Sign In</button>
+            </div>
+            <!--Grid column-->
+
+          </div>
+          <!--Grid row-->
+
+          <!--Grid row-->
+          <div class="row">
+
+            <!--Grid column-->
+            <div class="col-md-12">
+              <p class="font-small white-text d-flex justify-content-end">Don't Have an account? <a href="#" class="green-text ml-1 font-weight-bold">
+                  Sign Up</a></p>
+            </div>
+            <!--Grid column-->
+
+          </div>
+          <!--Grid row-->
+
+        </div>
+      </div>
+    </div>
+    </form>
+    <!--/.Content-->
+  </div>
+</div>
+<?php 
+   $username = $_SESSION['username'];
+   // Query untuk menampilkan data siswa berdasarkan NIS yang dikirim
+   $query ="SELECT * FROM penjualan  WHERE username_cs='".$username."'";
+   $sql = mysqli_query($koneksi, $query)  // Eksekusi/Jalankan query dari variabel $query
+   //$query ="SELECT * penjualan ";
+   //$sql = mysqli_query($koneksi, $query)  // Eksekusi/Jalankan query dari variabel $query
+?>
+<!-- Body -->
+<div class="container">
+<?php while($data = mysqli_fetch_array($sql)){?>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Kode Transaksi : <?php echo $data['kode_jual']; ?></h5>
+      <p class="card-text">Total Harga : Rp <?php echo $data['total_harga']; ?></p>
+      <p class="card-text">Tanggal : <?php echo $data['tanggal_jual']; ?></p>
+      <a href="#collapseExample<?php echo $data['kode_jual']; ?>" class="btn btn-primary" data-toggle="collapse" >Lihat Detail</a>
+    </div>
+    <div class="collapse" id="collapseExample<?php echo $data['kode_jual']; ?>">
+      <div class="collapse-content">
+        <p id="collapseExample">Detail Pembelian </p>
+        <table>
+        <tr>
+        <th>Nama Produk</th>
+        <th>Toko</th>
+        <th>Harga Produk</th>
+        </tr>
+     <?php   
+     $query1 ="SELECT * FROM detail_penjualan inner join produk on detail_penjualan.kode_produk=produk.kode_produk where kode_jual='".$data['kode_jual']."' ";
+    $sql1 = mysqli_query($koneksi, $query1);  // Eksekusi/Jalankan query dari variabel $query
+    while($data1 = mysqli_fetch_array($sql1)){
+    ?>
+        <tr>
+        <th><?php echo $data1['nama_produk']; ?></th>
+        <th><?php echo $data1['username_bs']; ?></th>
+        <th>Rp.<?php echo $data1['harga_produk']; ?></th>
+        </tr>
+    <?php }?>
+        </table>
+      </div>
+    </div>
+
+  </div>
+  <?php }?>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Kode Transaksi</h5>
+      <p class="card-text">Harga : Rp 200.000</p>
+      <p class="card-text">Tanggal : 12-12-2019</p>
+      <a href="#collapseExample2" class="btn btn-primary" data-toggle="collapse" >Button</a>
+    </div>
+    <div class="collapse" id="collapseExample2">
+      <div class="collapse-content">
+        <p id="collapseExample1">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- collapse -->
+
+<!-- collapse -->
+<!-- body -->
+<!-- Modal -->
+<form action="Login/cekloginbs.php" method="POST">
+<div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header text-center">
+        <h4 class="modal-title w-100 font-weight-bold">Sign in</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body mx-3">
+        <div class="md-form mb-5">
+          <i class="fas fa-envelope prefix grey-text"></i>
+          <input type="username" id="defaultForm-email" class="form-control validate" name="username">
+          <label data-error="wrong" data-success="right" for="defaultForm-email">Username</label>
+        </div>
+
+        <div class="md-form mb-4">
+          <i class="fas fa-lock prefix grey-text"></i>
+          <input type="password" id="defaultForm-pass" class="form-control validate" name="password" >
+          <label data-error="wrong" data-success="right" for="defaultForm-pass">Your password</label>
+        </div>
+
+      </div>
+      <div class="modal-footer d-flex justify-content-center">
+        <button type="submit" class="btn btn-default">Login</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+<form action="Login/cekloginbm.php" method="POST">
+<div class="modal fade" id="modalForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header text-center">
+        <h4 class="modal-title w-100 font-weight-bold">Sign in</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body mx-3">
+        <div class="md-form mb-5">
+          <i class="fas fa-envelope prefix grey-text"></i>
+          <input type="username" id="defaultForm-email" class="form-control validate" name="username" >
+          <label data-error="wrong" data-success="right" for="defaultForm-email">Username</label>
+        </div>
+
+        <div class="md-form mb-4">
+          <i class="fas fa-lock prefix grey-text"></i>
+          <input type="password" id="defaultForm-pass" class="form-control validate" name="password" >
+          <label data-error="wrong" data-success="right" for="defaultForm-pass">Your password</label>
+        </div>
+
+      </div>
+      <div class="modal-footer d-flex justify-content-center">
+        <button type="submit" class="btn btn-default">Login</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/6b0af4bcb3.js" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ Dodhy/lihat_antrian.php
@@ -0,0 +1,308 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Viga&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web&display=swap" rel="stylesheet">
+
+    <!--My CSS-->
+    <link rel="stylesheet" href="lihat_antrian.css">
+    <title>Barberian</title>
+  </head>
+  <body>
+
+  <!--Navbar-->
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">Barberian</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+          <a class="nav-item nav-link " href="index.php">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="index.php">Tentang Kami</a>
+          <div class="dropdown">
+            <a class="nav-item nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Fitur Kami
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="katalog.php">Katalog</a>
+                    <a class="dropdown-item" href="caribarber.php">Cari Barbershop</a>
+                    <a class="dropdown-item" href="halproduk.php">Produk</a>
+                  </div>
+              </div>
+                <?php if(!isset($_SESSION['username'])){ ?>
+            <div class="dropdown">
+              <a class="nav-item nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Masuk
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="" data-toggle="modal" data-target="#darkModalForm">Masuk Sebagai Customer</a>
+                      <a class="dropdown-item" href="" data-toggle="modal" data-target="#modalLoginForm">Masuk Sebagai Barbershop</a>
+                      <a class="dropdown-item" href="" data-toggle="modal" data-target="#modalForm">Masuk Sebagai Barberman</a>
+                    </div>
+              </div>
+              <?php }else{
+            ?>
+            <div class="dropdown">
+              <a class="nav-item nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <?php echo $_SESSION['username']?>
+                    </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href='profilcs.php'>Profil</a>
+                    <a class="dropdown-item" href="" >History Order</a>
+                    <a class="dropdown-item" href="" >Lihat Antrian</a>
+                    <a class="dropdown-item" href="logout.php" >Logout</a>
+                  </div>
+              </div>
+              <?php }
+              ?>
+            </div>
+          </div>
+      </div>
+      </nav>
+  <!-- akhir Navbar -->
+ <!-- awal isi -->
+ <?php      
+    include "koneksi.php";
+     $query= "SELECT * from  antrian where username_cs='".$_SESSION['username']."'and status_antrian='belum'";
+     $result=mysqli_query($koneksi,$query)?>
+    <!-- Form -->
+    <form action="pprofilcs.php" method="POST" name="form1" enctype="multipart/form-data" class="mt-5" >
+    <div class="container">
+    <?php 
+     $data = mysqli_fetch_array($result);
+    ?>
+           <div class="form-group row">
+              <label for="nomor" class="col-sm-2 col-form-label">Nomor Antrianmu :</label>
+              <div class="col-sm-2">
+                <input type="text" class="form-control" name="nomor" disabled value="<?php echo $data['username_cs']; ?>" >
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="nomor2" class="col-sm-2 col-form-label">Nomor Antrian Saat : Ini</label>
+              <div class="col-sm-2">
+                <input type="text" class="form-control" name="nomor2" disabled value="<?php echo $data['no_antrian']; ?>">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="nama_barber" class="col-sm-2 col-form-label">Nama Barbershop :</label>
+              <div class="col-sm-2">
+                <input type="text" class="form-control" name="nama_barber" disabled value="<?php echo $data['username_bs']; ?>">
+              </div>
+            </div>
+            <div class="col-sm-10 offset-sm-2">
+                    <button type="submit" class="btn btn-danger mb-2">Batal</button>
+            </div>
+      
+    </div>
+    </form>
+ <!-- akhir -->
+    <!-- akhir Jumbotron -->
+    <!-- Modal -->
+<div class="modal fade" id="darkModalForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+aria-hidden="true">
+<div class="modal-dialog form-dark" role="document">
+  <!--Content-->
+  <div class="modal-content card card-image" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/pricing-table%20(7).jpg');">
+    <div class="text-white rgba-stylish-strong py-5 px-5 z-depth-4">
+      <!--Header-->
+      <div class="modal-header text-center pb-4">
+        <h3 class="modal-title w-100 white-text font-weight-bold" id="myModalLabel"><strong>SIGN</strong> <a
+            class="green-text font-weight-bold"><strong> UP</strong></a></h3>
+        <button type="button" class="close white-text" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <!--Body-->
+      <div class="modal-body">
+        <!--Body-->
+        <div class="md-form mb-5">
+          <input type="email" id="Form-email5" class="form-control validate white-text">
+          <label data-error="wrong" data-success="right" for="Form-email5">Your email</label>
+        </div>
+
+        <div class="md-form pb-3">
+          <input type="password" id="Form-pass5" class="form-control validate white-text">
+          <label data-error="wrong" data-success="right" for="Form-pass5">Your password</label>
+          <div class="form-group mt-4">
+            <input class="form-check-input" type="checkbox" id="checkbox624">
+            <label for="checkbox624" class="white-text form-check-label">Accept the<a href="#" class="green-text font-weight-bold">
+                Terms and Conditions</a></label>
+          </div>
+        </div>
+
+        <!--Grid row-->
+        <div class="row d-flex align-items-center mb-4">
+
+          <!--Grid column-->
+          <div class="text-center mb-3 col-md-12">
+            <button type="button" class="btn btn-success btn-block btn-rounded z-depth-1">Sign up</button>
+          </div>
+          <!--Grid column-->
+
+        </div>
+        <!--Grid row-->
+
+        <!--Grid row-->
+        <div class="row">
+
+          <!--Grid column-->
+          <div class="col-md-12">
+            <p class="font-small white-text d-flex justify-content-end">Have an account? <a href="#" class="green-text ml-1 font-weight-bold">
+                Log in</a></p>
+          </div>
+          <!--Grid column-->
+
+        </div>
+        <!--Grid row-->
+
+      </div>
+    </div>
+  </div>
+  <!--/.Content-->
+</div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="darkModalForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog form-dark" role="document">
+    <!--Content-->
+    <form action="/K3GOL_E/barberian/Login/ceklogin.php " method="POST">
+    <div class="modal-content card card-image" id="popup">
+      <div class="text-white rgba-stylish-strong py-5 px-5 z-depth-4">
+        <!--Header-->
+        <div class="modal-header text-center pb-4">
+          <h3 class="modal-title w-100 white-text font-weight-bold" id="myModalLabel"><strong>SIGN</strong> <a
+              class="green-text font-weight-bold"><strong> IN</strong></a></h3>
+          <button type="button" class="close white-text" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <!--Body-->
+        <div class="modal-body">
+          <!--Body-->
+          <div class="md-form mb-5">
+            <input type="username" name="username" class="form-control validate white-text">
+            <label data-error="wrong" data-success="right" for="Form-email5">Your email</label>
+          </div>
+
+          <div class="md-form pb-3">
+            <input type="password" name="password" class="form-control validate white-text">
+            <label data-error="wrong" data-success="right" for="Form-pass5">Your password</label>
+          </div>
+
+          <!--Grid row-->
+          <div class="row d-flex align-items-center mb-4">
+
+            <!--Grid column-->
+            <div class="text-center mb-3 col-md-12">
+              <button type="submit" class="btn btn-success btn-block btn-rounded z-depth-1">Sign In</button>
+            </div>
+            <!--Grid column-->
+
+          </div>
+          <!--Grid row-->
+
+          <!--Grid row-->
+          <div class="row">
+
+            <!--Grid column-->
+            <div class="col-md-12">
+              <p class="font-small white-text d-flex justify-content-end">Don't Have an account? <a href="#" class="green-text ml-1 font-weight-bold">
+                  Sign Up</a></p>
+            </div>
+            <!--Grid column-->
+
+          </div>
+          <!--Grid row-->
+
+        </div>
+      </div>
+    </div>
+    </form>
+    <!--/.Content-->
+  </div>
+</div>
+<!-- Modal -->
+<form action="Login/cekloginbs.php" method="POST">
+<div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header text-center">
+        <h4 class="modal-title w-100 font-weight-bold">Sign in</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body mx-3">
+        <div class="md-form mb-5">
+          <i class="fas fa-envelope prefix grey-text"></i>
+          <input type="username" id="defaultForm-email" class="form-control validate" name="username">
+          <label data-error="wrong" data-success="right" for="defaultForm-email">Username</label>
+        </div>
+
+        <div class="md-form mb-4">
+          <i class="fas fa-lock prefix grey-text"></i>
+          <input type="password" id="defaultForm-pass" class="form-control validate" name="password" >
+          <label data-error="wrong" data-success="right" for="defaultForm-pass">Your password</label>
+        </div>
+        <label>Don't have account?</label><a href="admin/regadmin.php">Sign Up</a>
+      </div>
+      <div class="modal-footer d-flex justify-content-center">
+        <button type="submit" class="btn btn-default">Login</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+<form action="Login/cekloginbm.php" method="POST">
+<div class="modal fade" id="modalForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header text-center">
+        <h4 class="modal-title w-100 font-weight-bold">Sign in</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body mx-3">
+        <div class="md-form mb-5">
+          <i class="fas fa-envelope prefix grey-text"></i>
+          <input type="username" id="defaultForm-email" class="form-control validate" name="username" >
+          <label data-error="wrong" data-success="right" for="defaultForm-email">Username</label>
+        </div>
+
+        <div class="md-form mb-4">
+          <i class="fas fa-lock prefix grey-text"></i>
+          <input type="password" id="defaultForm-pass" class="form-control validate" name="password" >
+          <label data-error="wrong" data-success="right" for="defaultForm-pass">Your password</label>
+        </div>
+
+      </div>
+      <div class="modal-footer d-flex justify-content-center">
+        <button type="submit" class="btn btn-default">Login</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+<!-- Modal -->
+        <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+  </html>
