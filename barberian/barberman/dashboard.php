5a1771734c64c0a378d7635d5d3b47b80c3f5907
RestuDianGunadi <resgun312@gmail.com>
2019-12-05 14:07:32 +0700

    barberman

--- barberian/barberman/dashboard.php
+++ barberian/barberman/dashboard.php
@@ -1,3 +1,9 @@
+<?php
+$koneksi     = mysqli_connect("localhost", "root", "", "penjualan");
+$bulan       = mysqli_query($koneksi, "SELECT bulan FROM penjualan WHERE tahun='2016' order by id asc");
+$penghasilan = mysqli_query($koneksi, "SELECT hasil_penjualan FROM penjualan WHERE tahun='2016' order by id asc");
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -9,7 +15,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="admin1.css">
     <script src="https://kit.fontawesome.com/ef8e8d5793.js" crossorigin="anonymous"></script>
-    <title>HISTORY</title>
+    <title>DASHBOARD</title>
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-light fixed-top">
@@ -34,10 +40,10 @@
         <div class="col-md-2,5 bg-dark mt-2 pt-4 ">
                 <ul class="nav flex-column">
                     <ul class="nav flex-column">
-                        <ul class="sidebar navbar-nav">     
+                        <ul class="sidebar navbar-nav"> 
                              <li class="nav-item">
-                                <a class="nav-link active text-white" href="dashboard.php"><i class="fas fa-shopping-cart mr-2"></i>DASHBOARD</a><hr class="bg-secondary">
-                             </li>
+                                  <a class="nav-link active text-white" href="dashboard.php"><i class="fas fa-shopping-cart mr-2"></i>DASHBOARD</a><hr class="bg-secondary">
+                             </li>    
                              <li class="nav-item">
                                   <a class="nav-link active text-white" href="Orderan.html"><i class="fas fa-tags mr-2"></i>LIHAT ORDERAN</a><hr class="bg-secondary">
                              </li>
@@ -50,37 +56,76 @@
                       </ul>
         </div>
         <div class="col-md-8 p-5 pt-2">
-          <h3><i class="fas fa-history mr-2"></i>HISTORY ORDER</h3><hr>
+          <h3><i class="fas fa-shopping-cart mr-2"></i>DASHBOARD</h3><hr>
           <table class="table table-striped table-bordered">
-            <thead>
-              <tr>
-                <th scope="col">NO</th>
-                <th scope="col">NAMA CUSTOMER</th>
-                <th scope="col">ORDERAN</th>
-                <th scope="col">HARGA</th>
-                <th scope="col">TANGGAL</th>
-                <th scope="col">STATUS ORDERAN</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>IFAR FATWA MAHENDRA</td>
-                <td>Cukur Rambut</td>
-                <td>Rp 25.000</td>
-                <td>2 Oktober 2019</TD>
-                <td>Diterima</td>
-            </tbody>
 
-            <tbody>
-                    <th scope="row">2</th>
-                    <td>Dodhy Kurnia</td>
-                    <td>Cukur Rambut</td>
-                    <td>Rp 25.000</td>
-                    <td>2 Oktober 2019</TD>
-                    <td>Diterima</td>
-            </tbody>
-          </table>
+         <center><h1>GRAFIK PENJUALAN</h1></center>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.3.0/Chart.bundle.js"></script>
+        <script src="Chart.bundle.js"></script>
+        <style type="text/css">
+            .container {
+                width: 75%;
+                margin: 15px auto;
+            }
+        </style>
+
+        <div class="container">
+            <canvas id="myChart" width="100" height="100"></canvas>
+        </div>
+        <script>
+            var ctx = document.getElementById("myChart");
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: [<?php while ($b = mysqli_fetch_array($bulan)) { echo '"' . $b['bulan'] . '",';}?>],
+                    datasets: [{
+                            label: '# of Votes',
+                            data: [<?php while ($p = mysqli_fetch_array($penghasilan)) { echo '"' . $p['hasil_penjualan'] . '",';}?>],
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)',
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255,99,132,1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)',
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderWidth: 1
+                        }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                    }
+                }
+            });
+        </script>
+    </body>
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
