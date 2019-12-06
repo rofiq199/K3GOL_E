ed050c51da2dcf90ba3d62971c1f40c8a95395b3
dodhy <dodhyk29@gmail.com>
2019-12-06 20:20:47 +0700

    Update antrian.php

--- barberian/Admin/antrian.php
+++ barberian/Admin/antrian.php
@@ -60,7 +60,7 @@
         </div>
         <div class="col-md-8 p-5 pt-2">
           <h3><i class="fas fa-money-check mr-2"></i> Lihat Antrian</h3><hr>
-          <a href="#" class="btn btn-primary mb-3"><i class="fas fa-plus-square mr-2"></i>Tambah Antrian</a>
+          <a href="#modalplus" class="btn btn-primary mb-3" data-toggle="modal"><i class="fas fa-plus-square mr-2"></i>Tambah Antrian</a>
           <table class="table table-striped table-bordered">
             <thead>
               <tr>
@@ -89,6 +89,35 @@
                 <?php } ?>
               </tr>
         <?php }?>
+
+        <form  action="" method="" enctype="multipart/form-data">
+      <div class="modal fade" id="modalplus" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header text-center">
+              <h4 class="modal-title w-100 font-weight-bold">Tambah Antrian
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body mx-2 my-3">
+              <div class="md-form mb-2">
+                <input type="username" id="nomor_antrian" class="form-control validate"  name="antri1" placeholder="Nomor Antrian">
+              </div>
+              <div class="md-form mb-2">
+                <input type="username" id="nama" class="form-control validate" name="nama1" placeholder="Nama Pelanggan">
+              </div>
+              
+            </div>
+            <div class="modal-footer d-flex justify-content-center">
+                  <button type="submit" class="btn btn-primary">Tambah</button>
+                  <button type="button" class="btn btn-danger" data-dismiss="modal">Batal</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      </form>
             </tbody>
           </table>
 
