0548e3f62f190ab53d0253c0d5f2ecacbae4b2c1
ramma99 <55478759+ramma99@users.noreply.github.com>
2019-12-18 06:47:42 +0700

    baru

---
+++ barberian/halamanpesan.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  <?php 
+    include "navbar.php";
+  ?>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="halamanpesan.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Viga&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/6b0af4bcb3.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8">
+                <div class="card">
+                    <div class="card-header">
+                        <h5>Kode Pemesanan</h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">Nama Barberman</p>
+                        <p class="card-text">Alamat</p>
+                        <p class="card-text">Jam Pesan</p>
+                        <div class="card mb-2">
+                            <div class="card-header">
+                            <table cellspacing="2" width="85%">
+                                <tr>
+                                    <td>Nama Layanan</td>
+                                    <td>Harga</td>
+                                </tr>
+                            </table>
+                            </div>
+                            <div class="card-body">
+                            <table cellspacing="2" width="85%">
+                                <tr>
+                                    <td>Nama Layanan</td>
+                                    <td>Harga</td>
+                                </tr>
+                            </table>
+                            </div>
+                        </div>
+                        <center><a href="#" class="btn btn-success">Bayar</a></center>
+                    </div>
+                </div>
+            
+            </div>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
