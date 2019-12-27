36811961e719bebcf209ea2b96c43abcc2777ac7
Dodhy Kurnia Rahmantyo <dodhyk29@gmail.com>
2019-12-27 20:22:10 +0700

    baru

--- barberian/barbermanbaru/sidebar.php
+++ barberian/barbermanbaru/sidebar.php
@@ -1,7 +1,12 @@
 <link rel="stylesheet" href="sidebar.css">
 <?php session_start(); ?>
    <div class="wrapper">
+       
    	<nav id="sidebar">
+           <div class="sidebar-header">
+              Barberian 
+           </div>
+           <p>Username Login</p>
    		<ul class="list-unstyled components">
    			<li>
    				<a href="#">Ubah Profil</a>
