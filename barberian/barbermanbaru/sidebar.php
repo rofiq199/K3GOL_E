4661b46577daaa9cd819540e2bb536815b451bbc
Dodhy Kurnia Rahmantyo <dodhyk29@gmail.com>
2019-12-27 16:37:10 +0700

    baru
    halaman baru buat barberman

---
+++ barberian/barbermanbaru/sidebar.php
@@ -0,0 +1,34 @@
+<link rel="stylesheet" href="sidebar.css">
+<?php session_start(); ?>
+   <div class="wrapper">
+   	<nav id="sidebar">
+   		<ul class="list-unstyled components">
+   			<li>
+   				<a href="#">Ubah Profil</a>
+   			</li>
+   			<li>
+   				<a href="#">Lihat Pesanan</a> 
+   			</li>
+   			<li>
+   				<a href="#">Histori Pemesanan</a>
+   			</li>
+   		</ul>	
+   	</nav>
+   	
+   	<div class="content">
+   		<nav class="navbar navbar-expand-lg">
+   			<i class="fas fa-align-justify" id="sidebarCollapse"></i>
+   		
+  <!--<a class="navbar-brand" href="#">Navbar</a> -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><i class="far fa-bell"></i></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</div>
