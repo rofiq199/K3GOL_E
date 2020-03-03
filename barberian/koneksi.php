e4f1d8c1c0b305ff3c3bbafd36e85522ef7663a3
rofiq199 <55471142+rofiq199@users.noreply.github.com>
2020-03-03 21:47:04 +0700

    a

--- barberian/koneksi.php
+++ barberian/koneksi.php
@@ -1,5 +1,5 @@
 <?php 
-$koneksi = mysqli_connect("localhost","root","","barberian");
+$koneksi = mysqli_connect("localhost","u5445042_barberian","fifaonline3","u5445042_barberian");
 // Check connection
 if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
